adbf8ed947a0b0dfebbe11e0971cb26fa4396c79
Chaoozng Sun <suncz@zonekey.com.cn>
2017-12-14 11:39:15 +0800

    First version.

---
+++ H264VedioFrameRender/H264VedioFrameRender.cpp
@@ -0,0 +1,285 @@
+//#pragma comment(lib, "d3d9.lib")
+
+#include "H264VedioFrameRender.h"
+
+char filepath_in[] = "D:/temp/720.h264";
+
+//For test in application mode.
+int main(int argc, char* argv[])
+{
+	InitDecoder((HWND)198886, 30, 0, 0);
+
+	read264File();
+
+	ReleaseDecoder();
+}
+
+//For test.
+void read264File()
+{
+	const int in_buffer_size = 4096;
+
+	int size;
+	byte buffer[in_buffer_size + FF_INPUT_BUFFER_PADDING_SIZE] = { 0 };
+
+	FILE *fp_in;
+
+	fp_in = fopen(filepath_in, "rb");
+	if (!fp_in) {
+		printf("Could not open input stream\n");
+		return;
+	}
+
+	while (IsWorking) {
+		size = fread(buffer, 1, in_buffer_size, fp_in);
+		if (size == 0) {
+			break;
+		}
+		FeedDecoder(buffer, size);
+	}
+
+	fclose(fp_in);
+}
+
+IDirect3D9* _d3d = NULL;
+IDirect3DDevice9* _device = NULL;
+IDirect3DSurface9* _surface = NULL;
+
+void releaseRender()
+{
+	if (_surface)
+		_surface->Release();
+	if (_device)
+		_device->Release();
+	if (_d3d)
+		_d3d->Release();
+}
+
+//todo: try the dangerous part.
+int initRender()
+{
+	HRESULT lRet;
+	releaseRender();
+
+	_d3d = Direct3DCreate9(D3D_SDK_VERSION);
+	if (_d3d == NULL)
+	{
+		return -1;
+	}
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+
+	GetClientRect(VideoRenderHandle, &VideoViewport);
+	adjustRenderSize(pCodecCtx->width, pCodecCtx->height);
+
+	lRet = _d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, VideoRenderHandle, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &_device);
+	if (FAILED(lRet))
+		return -1;
+
+	lRet = _device->CreateOffscreenPlainSurface(
+		pCodecCtx->width, pCodecCtx->height,
+		RenderFormat,
+		D3DPOOL_DEFAULT,
+		&_surface,
+		NULL);
+
+	if (FAILED(lRet))
+		return -1;
+
+	return 0;
+}
+
+void adjustRenderSize(int videoWidth, int videoHeight)
+{
+	int w, h;
+
+	if (MulDiv(videoWidth, VideoViewport.bottom - VideoViewport.top, videoHeight) <= VideoViewport.right - VideoViewport.left)
+	{
+		w = MulDiv(VideoViewport.bottom - VideoViewport.top, videoWidth, videoHeight);
+		h = VideoViewport.bottom - VideoViewport.top;
+	}
+	else
+	{
+		w = VideoViewport.right - VideoViewport.left;
+		h = MulDiv(VideoViewport.right - VideoViewport.left, videoHeight, videoWidth);
+	}
+
+	VideoRenderArea.left = VideoViewport.left + (VideoViewport.right - VideoViewport.left - w) / 2;
+	VideoRenderArea.top = VideoViewport.top + (VideoViewport.bottom - VideoViewport.top - h) / 2;
+	VideoRenderArea.right = VideoRenderArea.left + w;
+	VideoRenderArea.bottom = VideoRenderArea.top + h;
+}
+
+bool render(AVFrame* const pFrame)
+{
+	HRESULT lRet;
+	if (_surface == NULL) {
+		return false;
+	}
+	D3DLOCKED_RECT d3d_rect;
+	lRet = _surface->LockRect(&d3d_rect, NULL, D3DLOCK_DONOTWAIT);
+	if (FAILED(lRet)) {
+		return false;
+	}
+
+	byte * pDest = (BYTE *)d3d_rect.pBits;
+	int stride = d3d_rect.Pitch;
+
+	int	w2 = pFrame->width / 2, h2 = pFrame->height / 2;
+	int s2 = stride / 2;
+	//Tip: I don't know which is u or v, so I guessed and created the variant names: dpu,dpv,spu,spv.
+	int dpy = 0, dpu = dpy + stride * pFrame->height, dpv = dpu + s2 * h2;
+	int spy = 0, spu = 0, spv = 0;
+
+	for (int i = 0; i < pFrame->height; i++) {
+		memcpy(pDest + dpy, pFrame->data[0] + spy, pFrame->width);
+		dpy += stride;
+		spy += pFrame->linesize[0];
+
+		if (i < h2) {
+			memcpy(pDest + dpv, pFrame->data[1] + spu, w2);
+			dpv += s2;
+			spu += pFrame->linesize[1];
+			memcpy(pDest + dpu, pFrame->data[2] + spv, w2);
+			dpu += s2;
+			spv += pFrame->linesize[2];
+		}
+	}
+
+	lRet = _surface->UnlockRect();
+	if (FAILED(lRet)) {
+		return false;
+	}
+
+	if (_device == NULL) {
+		return false;
+	}
+
+	_device->Clear(0, NULL, D3DCLEAR_TARGET, VideoBackColor, 1.0f, 0);
+	_device->BeginScene();
+	IDirect3DSurface9 * pBackBuffer = NULL;
+	_device->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &pBackBuffer);
+	_device->StretchRect(_surface, NULL, pBackBuffer, &VideoRenderArea, D3DTEXF_LINEAR);
+	_device->EndScene();
+	_device->Present(NULL, NULL, NULL, NULL);
+
+	return true;
+}
+
+//todo: try the dangerous part.
+bool InitDecoder(HWND const videoRenderHandle, int const frameRate, int const videoWidth, int const videoHeight)
+{
+	avcodec_register_all();
+
+	pCodec = avcodec_find_decoder(codec_id);
+	if (!pCodec) {
+		printf("Codec not found\n");
+		return false;
+	}
+	pCodecCtx = avcodec_alloc_context3(pCodec);
+	if (!pCodecCtx) {
+		printf("Could not allocate video codec context\n");
+		return false;
+	}
+
+	if (videoWidth > 0) {
+		pCodecCtx->width = 1280;
+		pCodecCtx->coded_width = 1280;
+	}
+
+	if (videoHeight > 0) {
+		pCodecCtx->height = 720;
+		pCodecCtx->coded_height = 720;
+	}
+
+	pCodecCtx->pix_fmt = DecoderOutputType;
+
+	pCodecParserCtx = av_parser_init(codec_id);
+	if (!pCodecParserCtx) {
+		printf("Could not allocate video parser context\n");
+		return false;
+	}
+
+	if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+		printf("Could not open codec\n");
+		return false;
+	}
+	_decodedFrame = av_frame_alloc();
+
+	VideoRenderHandle = videoRenderHandle;
+
+	VideoFrameRate = frameRate == 0 ? DefaultFrameRate : frameRate;
+	if (VideoFrameRate > 1000) {
+		VideoFrameRate = 1000;
+	}
+	_frameTime = 1000 / frameRate;
+
+	_isFirstFrame = true;
+	IsWorking = true;
+
+	return true;
+}
+
+//todo: buffer maybe unsafe or not accessable, so..
+void FeedDecoder(byte* const buffer, int const size)
+{
+	AVPacket packet;
+
+	av_init_packet(&packet);
+
+	byte* bufferPosition = buffer;
+	int leftSize = size;
+
+	int decodeResult = 0, got_picture = 0;
+
+	while (IsWorking && leftSize > 0) {
+		int len = av_parser_parse2(pCodecParserCtx, pCodecCtx, &packet.data, &packet.size, bufferPosition, leftSize,
+			AV_NOPTS_VALUE, AV_NOPTS_VALUE, AV_NOPTS_VALUE);
+
+		bufferPosition += len;
+		leftSize -= len;
+
+		if (packet.size == 0) {
+			continue;
+		}
+
+		decodeResult = avcodec_decode_video2(pCodecCtx, _decodedFrame, &got_picture, &packet);
+		if (decodeResult <= 0 || got_picture == 0) {
+			continue;
+		}
+		if (_isFirstFrame) {
+			_isFirstFrame = false;
+			initRender();
+		}
+
+		//todo: We should recreate render device when the size of viewport or rendering image is changed.
+		render(_decodedFrame);
+
+		if (_frameTime > 0) {
+			Sleep(_frameTime);
+		}
+	}
+}
+
+void ReleaseDecoder()
+{
+	IsWorking = false;
+
+	releaseRender();
+
+	if (pCodecParserCtx)
+		av_parser_close(pCodecParserCtx);
+
+	if (pCodecCtx)
+		avcodec_close(pCodecCtx);
+
+	if (pCodecCtx)
+		av_free(pCodecCtx);
+
+	if (_decodedFrame)
+		av_frame_free(&_decodedFrame);
+}
---
+++ H264VedioFrameRender/H264VedioFrameRender.h
@@ -0,0 +1,86 @@
+/**
+* Please leave this part, in memory of:
+* 雷霄骅
+* 中国传媒大学/数字电视技术
+*
+* Source code is get from here:
+* http://blog.csdn.net/leixiaohua1020/article/details/40279297
+* http://blog.csdn.net/leixiaohua1020/article/details/42181571
+*/
+
+#pragma once
+#include <stdio.h>
+#include <tchar.h>
+#include <d3d9.h>
+#include <windows.h>  
+
+#define __STDC_CONSTANT_MACROS
+extern "C"
+{
+#include "libavcodec/avcodec.h" 
+};
+
+const AVCodecID codec_id = AV_CODEC_ID_H264;
+const AVPixelFormat DecoderOutputType = AV_PIX_FMT_YUV420P;
+
+const D3DFORMAT RenderFormat = (D3DFORMAT)MAKEFOURCC('Y', 'V', '1', '2');
+const int BitsPerPoint_YV12 = 12;
+
+const int DefaultFrameRate = 30;
+
+HWND VideoRenderHandle;
+//Total render area in render target.
+RECT VideoViewport;
+//Current render area in render viewport.
+RECT VideoRenderArea;
+int VideoFrameRate = DefaultFrameRate;
+
+D3DCOLOR VideoBackColor = D3DCOLOR_XRGB(0, 0, 0);
+//Set false when want to stop decoding work.
+bool IsWorking = true;
+
+//private fields.
+AVCodec *pCodec;
+AVCodecContext *pCodecCtx = NULL;
+AVCodecParserContext *pCodecParserCtx = NULL;
+
+//For internal use only, this field is used to contain decoded frame while decoding video.
+AVFrame	*_decodedFrame;
+//For internal use only, will init render on first frame.
+bool _isFirstFrame = true;
+//For internal use only, this field is only calculated on frame rate is set on init().
+int _frameTime = 1000 / VideoFrameRate;
+
+int initRender();
+//Adjust the VideoRenderArea in VideoViewport, will keep the w-h ratio while adjusting.
+void adjustRenderSize(int videoWidth, int videoHeight);
+bool render(AVFrame* const pFrame);
+void releaseRender();
+
+/* Initialize decoder.
+@warning This function is not thread safe!
+
+@param videoRenderHandle, handle of the render target.
+@param frameRate frame rate, max value is 1000.
+		Fill ZERO when use default(30),.
+		Fill negative value to use realtime rendering, then Render will not wait interval time between frames.
+@param videoWidth width of the video, fill ZERO when does not know.
+@param videoHeight height of the video, fill ZERO when does not know.
+*/
+extern "C" _declspec(dllexport) bool InitDecoder(HWND const videoRenderHandle, int const frameRate, int const videoWidth, int const videoHeight);
+
+/* Feed decoder with H264 frame buffer, it will render when frame is decoded.
+@warning This function is not thread safe!
+
+@param Buffer address of H264 video buffer.
+@param Byte size of the 264 data in buffer.
+*/
+extern "C" _declspec(dllexport) void FeedDecoder(byte* const buffer, int const size);
+
+/* Release decoder and render.
+@warning This function is not thread safe!
+*/
+extern "C" _declspec(dllexport) void ReleaseDecoder();
+
+//For test.
+void read264File();
