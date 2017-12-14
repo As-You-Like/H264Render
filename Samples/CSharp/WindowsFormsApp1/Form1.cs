6efd43df1e5aa10b6095f2ac4a51d80052b5a3bd
Chaoozng Sun <suncz@zonekey.com.cn>
2017-12-14 12:06:47 +0800

    Add sample for C#.

---
+++ Samples/CSharp/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const string H264File = "D:\\temp\\720.h264";
+
+        bool _isRendering;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            H264Player.Init(panel1.Handle, 30, 0, 0);
+            new Thread(new ThreadStart(readBuffer)).Start();
+        }
+
+        private void readBuffer()
+        {
+            var file = File.OpenRead(H264File);
+
+            var buffer = new byte[4096];
+            var position = 0;
+            var len = 0;
+
+            _isRendering = true;
+            while (_isRendering && (len = file.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                position += len;
+                H264Player.Decode(buffer, len);
+            }
+            H264Player.Release();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _isRendering = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Samples/CSharp/WindowsFormsApp1/H264Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    internal class H264Player
+    {
+        const string H264Decoder = "H264VedioFrameRender.dll";
+
+        /// <summary>
+        /// Initialize the decoder and render.
+        /// </summary>
+        /// <param name="videoRenderHandle">Handle will render video on.</param>
+        /// <param name="frameRate">Frame rate of video, Zero for default(30), nagetive for realtime(not interval between frames).</param>
+        /// <param name="videoWidth">Fill video width if konw, or fill ZERO.</param>
+        /// <param name="videoHeight"Fill video height if konw, or fill ZERO.></param>
+        /// <returns>True when decoder is initialized.</returns>
+        /// 
+        /// <remarks>
+        /// This method is not thread safe, do not try to <see cref="Init(IntPtr, int, int, int)"/> while decoding or rending.
+        /// </remarks>
+        [DllImport(H264Decoder, EntryPoint = "InitDecoder", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool Init(IntPtr videoRenderHandle, int frameRate, int videoWidth, int videoHeight);
+
+        /// <summary>
+        /// Decode data in buffer and render decoded frame on Render handle.
+        /// </summary>
+        /// <param name="buffer">H264 buffer.</param>
+        /// <param name="size">Buffer data size in buffer.</param>
+        /// 
+        /// <remarks>
+        /// This method is not thread safe, make sure<see cref="Init(IntPtr, int, int, int)"/> is called and <see cref="Release"/> must not be called when decode video buffer.
+        /// </remarks>
+        [DllImport(H264Decoder, EntryPoint = "FeedDecoder", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+
+        public static extern void Decode(byte[] buffer, int size);
+
+        /// <summary>
+        /// Release render and decoder.
+        /// </summary>
+        /// <remarks>
+        /// This method is not thread safe, you must stop <see cref="Decode/> before <see cref="Release"/>.
+        /// </remarks>
+        [DllImport(H264Decoder, EntryPoint = "ReleaseDecoder", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Release();
+    }
+}
